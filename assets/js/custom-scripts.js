91314eea8f6068f4ef8122f92f1557eb605f7675
chjoguer <cjguerrero_garcia@outlook.com>
2020-05-29 11:17:02 -0500

    Auto-generated commit

---
+++ assets/js/custom-scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top) 
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
